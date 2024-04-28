99bad55c57bf22a7160286f36cf1fdf1d10802c4
Albrict <albrict.work@gmail.com>
2024-04-28 18:58:10 +0200

    Added basic main cycle and some macros

--- pong.asm
+++ pong.asm
@@ -1,23 +1,32 @@
-extern	printf		; the C function, to be called
-extern  InitWindow
-global main		    ; the standard gcc entry point
+%include "raylib.inc"
 
-section .data		        ; Data section, initialized variables
-width  dd 800
-height dd 600
-title  db "Hello from NASM"
+global main	; the standard gcc entry point
 
-section .text           ; Code section.
-main:				    ; the program label for the entry point
-    push    rbp		    ; set up stack frame, must be alligned
-	
-	mov	rdi, [width]
-	mov	rsi, [height]
-    mov rdx, title
-	mov	rax, 0		    ; or can be  xor  rax,rax
-    call    InitWindow  ; Call C function
+section .rodata
+; Game variables(window, fps and etc)
+width        dd 800               ; Window width
+height       dd 600               ; Window height
+fps          dd 60                ; FPS
+field_color  dd 0x759D33          ; Background color
+title        db "Hello from NASM", 0 ; Window title
 
-	pop	rbp		        ; restore stack
-
-	mov	rax,0		    ; normal, no error, return value
-	ret			        ; return
+; Game objects
+object_color dd 0xFFFFFFFF      ; Color of rackets and ball
+left_racket  dd 0, 0, 20, 100 ; Left racket rectangle 
+right_racket dd width - 20, 0, 20, 100
+section .text ; Code section.
+main:				                                  ; libc entry point
+    init_window    [width], [height], title           ; Init raylib window
+    set_target_fps [fps]                              ; Set fps
+game_cycle:                                           ; Main game cycle
+    begin_drawing                                     ; Start drawing
+        clear_background [field_color]                ; Clear background of window
+        draw_rectangle   left_racket,  [object_color] ; Draw left racket
+        draw_rectangle   right_racket, [object_color]
+    end_drawing                                       ; End drawing
+    window_should_close                               ; Is window closed
+    test eax, eax 
+    jz   game_cycle                                   ; Not closed - continue game cycle
+end:
+	mov	rax,0		                                  ; normal, no error, return value
+	ret			                                      ; return
